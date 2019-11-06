764bcaa0818527ffcaba4463fa8d3c3a21e80f1d
Caio Ruman <caioruman@gmail.com>
2019-11-06 14:31:21 -0500

    fourth test

--- calc-inv.py
+++ calc-inv.py
@@ -17,24 +17,54 @@ import argparse
     Using the
 '''
 
-parser=argparse.ArgumentParser(description='Histograms of the inversions', formatter_class=argparse.RawTextHelpFormatter)
+#parser=argparse.ArgumentParser(description='Histograms of the inversions', formatter_class=argparse.RawTextHelpFormatter)
 #parser.add_argument("-op", "--opt-arg", type=str, dest='opcional', help="Algum argumento opcional no programa", default=False)
-parser.add_argument("exp", type=str, help="Ano", default=0)
-args=parser.parse_args()
+#parser.add_argument("exp", type=str, help="Ano", default=0)
+#args=parser.parse_args()
 
-exp = args.exp
-exp = 'PanArctic_0.5d_CanHisto_NOCTEM_RUN'
+#exp = args.exp
 
-def main(exp):    
+'''
+  to do: Get the land/ocean mask and do a histrogram for each
+'''
+
+def main():    
 
     main_folder = "/home/cruman/projects/rrg-sushama-ab/teufel/{0}".format(exp)
+
+    exp = ['PanArctic_0.5d_CanHisto_NOCTEM_RUN', 'PanArctic_0.5d_CanHisto_NOCTEM_R2', 'PanArctic_0.5d_CanHisto_NOCTEM_R3',
+       'PanArctic_0.5d_CanHisto_NOCTEM_R5', 'PanArctic_0.5d_CanHisto_NOCTEM_R4']
 
     datai = 1976
     dataf = 2005
     
     deltaT_l = []
 
-    for year in range(datai, dataf+1):
+    deltaT_R1 = calcInversions(exp[0], datai, dataf, main_folder)
+    #deltaT_R2 = calcInversions(exp[1], datai, dataf, main_folder)
+    #deltaT_R3 = calcInversions(exp[2], datai, dataf, main_folder)
+    #deltaT_R4 = calcInversions(exp[3], datai, dataf, main_folder)
+    #deltaT_R5 = calcInversions(exp[4], datai, dataf, main_folder)    
+
+    fname = 'histogram_R1.png'
+
+    bins = np.arange(-20,21,1)
+
+    calc_histogram(deltaT_R1, bins)
+
+
+def calc_histogram(data, bins):
+
+  hist, bin_edges = np.histogram(data, bins)
+  print(bin_edges)
+  print(hist)
+
+  # plot the histrogram
+  sys.exit()
+
+def calcInversions(exp, datai, dataf, main_folder):
+
+  for year in range(datai, dataf+1):
 
       for month in range(1,13):
 
@@ -53,20 +83,16 @@ def main(exp):
 
           lons2d, lats2d = r.get_longitudes_and_latitudes_for_the_last_read_rec() 
 
-          deltaT[lats2d < 64] = np.nan
+          deltaT[lats2d < 64.] = np.nan
           aux = deltaT.flatten()
-          deltaT_l.extent(aux[~np.isnan(aux)].tolist())
-          print(deltaT_l)
-          print(len(deltaT_l))
-          
-        
-      print(deltaT_l)
-      print(len(deltaT_l))
-      sys.exit()
-          
+          deltaT_l.extend(aux[~np.isnan(aux)].tolist())
+          #print(deltaT_l)
+          #print(len(deltaT_l))
+                  
+      #print(deltaT_l)
+      #print(len(deltaT_l))
 
-        #store the data to create the histogram at the end of the loop
-    
+      return deltaT_l
 
 if __name__ == "__main__":
     main(exp)
